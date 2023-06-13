0f66c5afae94c5c19227b2469e42e9694667cfcb
MukeshKaripe <karipemukesh@gmail.com>
2023-06-13 19:27:07 +0530

    latest-build-important

--- app.js
+++ app.js
@@ -13,8 +13,11 @@ const DUMMY_DATA = [
 // .enter()
 // .append('p')
 // .text(dta => dta.country);
- 
- const container = d3.select('div')
+const xscale = d3.scaleBand().domain(DUMMY_DATA.map(datapoint => datapoint.country)).rangeRound([0,250]).padding(0.1);
+const yscale = d3.scaleLinear().domain([0,12]).range([500,0]);
+
+
+ const container = d3.select('svg')
 .classed('class',true)
 .style('background','blue');
 
@@ -22,11 +25,14 @@ const DUMMY_DATA = [
 
 const bar =  container.selectAll('.bar').data(DUMMY_DATA)
 .enter()
-.append('div')
+.append('rect')
 .classed('bar',true)
-.style('background','red')
-// .style('width','40px')
+// .style('background','red')
+.style('fill','red')
+.attr('width', xscale.bandwidth())
+// width is calculated by positions and sizes not by width and height in d3 (browser ugives auto width by d3 libraries.)
 // .style('height','auto')
-.style('height',data => (data.value * 15) + 'px' )
-
-      ;
+// .style('height',data => (data.value * 15) + 'px' )
+.style('height',data => 500 - yscale( data.value))
+ .attr('x', data => xscale(data.country))
+ .attr('y', data => yscale(data.value)) ;
